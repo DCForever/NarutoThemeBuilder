dc039805b4ffc1eca3de05ba3205373dcaf0c311
Daniel Clark <daniel.clark@zumobi.com>
2021-05-12 08:36:33 -0700

    Initial commit of Xcode project

---
+++ Naruto Theme Downloader/Shared/Naruto_Theme_DownloaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Naruto_Theme_DownloaderApp.swift
+//  Shared
+//
+//  Created by Daniel Clark on 5/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Naruto_Theme_DownloaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
